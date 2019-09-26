c6f66eb9e395e17f7686f1a135afd9cbc9809da5
William Pollock <wmpollock@gmail.com>
2019-09-26 14:50:13 -0700

    more data extractions.

--- metafodder.py
+++ metafodder.py
@@ -2,8 +2,10 @@
 # Bill Pollock
 # 2019
 
-import clean_filename, feedparser, re, sys
+import clean_filename, feedparser, os, re, urllib.parse, sys
 
+# Unfortunately necessary if you redirect STDOUT w/o barfing 
+sys.stdout.reconfigure(encoding='utf-8')
 
 def process_feed(feed_url):
     rss_playlist = feedparser.parse(feed_url)
@@ -17,16 +19,29 @@ def process_feed(feed_url):
         # This parse is biased towards titles and against people with by in their name.
         # If you favorite "Two if by sea" by Paul by the Horse, expect sorrow.
         m = re.match(r"^(.+) by (.+?)$", entry['title'])
-        title = m.group(1)    
+        title = m.group(1)
         artist = m.group(2)
         # I mean, literally I'm doing the same horrible thing here with my hyphen but
         # at least you'll have sensible MP3 tags there, kinda.
-        filename = clean_filename.convert(artist + " - " + title)
-       
+
+        # Also I thought maybe MeFi would be progressively using OGG but as of 2019
+        # their submissions are still.  MP3 only ¯\_(ツ)_/¯  I mean, yeet, I guess.
+        # Thought I was going to have go get all up into mimetypes.guess_extension :O
+        filename = clean_filename.convert(artist + " - " + title) + ".mp3"
+
+        # Feedparser considers this an "unusual" element. 
+        url = entry.enclosures[0]['href'];
+        urlparts = urllib.parse.urlparse(url)
+        url_filename  = urllib.parse.unquote(os.path.basename(urlparts.path))
+
         print("Title: ",  title)
         print("Artist: ", artist)
         print("Filename: ", filename)
+        print("Link: ", entry["link"])
+        print("URL: ", url);
+        print("URL filename: ", url_filename)
         print("---------")
 
-process_feed(sys.argv[1])
+for arg in sys.argv[1:]:
+    process_feed(arg)
 
